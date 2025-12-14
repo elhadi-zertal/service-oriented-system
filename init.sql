bb483bf0a6c6edf78fabe3464b53c022ed2c8073
elhadi-zertal <med.zertal@gmail.com>
2025-12-14 12:56:13 +0100

    check point

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS system_logs(
+    id SERIAL PRIMARY KEY,
+    service_name VARCHAR(15) NOT NULL,
+    log_message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+INSERT INTO system_logs(service_name, log_message) VALUES
+('nginx', 'reverse proxy started'),
+('backend', 'API service initialized'),
+('postgres', 'database connection established');
+
+
